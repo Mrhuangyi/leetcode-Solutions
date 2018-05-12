48ca604a2960e348e0a146719d9c600eb4774bb1
Mr.Huang <18757163093@163.com>
2018-05-12 21:40:30 +0800

    Update 69. x 的平方根.cpp

--- Cpp/Easy/69. x 的平方根.cpp
+++ Cpp/Easy/69. x 的平方根.cpp
@@ -21,3 +21,24 @@ public:
         return sqrt(x);
     }
 };
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left = 1,right = x/2;
+        int last_mid;
+        if(x<2) return x;
+        while(left<=right){
+            const int mid = left+(right-left)/2;
+            if(x/mid>mid){
+                left = mid+1;
+                last_mid = mid;
+            }else if(x/mid<mid){
+                right = mid-1;
+            }else{
+                return mid;
+            }
+        }
+        return last_mid;
+    }
+};
