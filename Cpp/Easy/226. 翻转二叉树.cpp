016016b9400447673e488f04e43d8640146813df
Mr.Huang <18757163093@163.com>
2018-06-03 11:33:57 +0800

    Create 226. 翻转二叉树.cpp

---
+++ Cpp/Easy/226. 翻转二叉树.cpp
@@ -0,0 +1,81 @@
+翻转一棵二叉树。
+
+示例：
+
+输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+备注:
+这个问题是受到 Max Howell 的 原问题 启发的 ：
+
+谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+
+递归写法：
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL){
+            return NULL;
+        }
+        TreeNode* right = invertTree(root->right);
+        TreeNode* left = invertTree(root->left);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
+
+迭代写法：
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL){
+            return NULL;
+        }
+        queue<TreeNode* > q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode *current = q.front();
+            q.pop();
+            TreeNode *temp = current->left;
+            current->left = current->right;
+            current->right = temp;
+            if(current->left!=NULL){
+                q.push(current->left);
+            }
+            if(current->right!=NULL){
+                q.push(current->right);
+            }
+        }
+        return root;
+    }
+};
