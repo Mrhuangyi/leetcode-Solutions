33a072cec4c793a3c246fdaceb23de2dc8cef67f
Mr.Huang <18757163093@163.com>
2018-06-10 21:30:44 +0800

    Create 350. 两个数组的交集 II.cpp

---
+++ Cpp/Easy/350. 两个数组的交集 II.cpp
@@ -0,0 +1,38 @@
+给定两个数组，写一个方法来计算它们的交集。
+
+例如:
+给定 nums1 = [1, 2, 2, 1], nums2 = [2, 2], 返回 [2, 2].
+
+注意：
+
+   输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+   我们可以不考虑输出结果的顺序。
+跟进:
+
+如果给定的数组已经排好序呢？你将如何优化你的算法？
+如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+如果nums2的元素存储在磁盘上，内存是有限的，你不能一次加载所有的元素到内存中，你该怎么办？
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int len1 = nums1.size();
+        int len2 = nums2.size();
+        int i = 0,j = 0;
+        while(i<len1&&j<len2){
+            if(nums1[i]<nums2[j]){
+                i++;
+            }else if(nums1[i]>nums2[j]){
+                j++;
+            }else{
+                res.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        return res;
+    }
+};
