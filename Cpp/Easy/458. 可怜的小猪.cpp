837e4574147b5f32db6e06e59f7840a999522db8
Mr.Huang <18757163093@163.com>
2018-07-22 18:15:52 +0800

    Create 458. 可怜的小猪.cpp

---
+++ Cpp/Easy/458. 可怜的小猪.cpp
@@ -0,0 +1,16 @@
+有1000只水桶，其中有且只有一桶装的含有毒药，其余装的都是水。它们从外观看起来都一样。如果小猪喝了毒药，它会在15分钟内死去。
+
+问题来了，如果需要你在一小时内，弄清楚哪只水桶含有毒药，你最少需要多少只猪？
+
+回答这个问题，并为下列的进阶问题编写一个通用算法。
+
+进阶:
+
+假设有 n 只水桶，猪饮水中毒后会在 m 分钟内死亡，你需要多少猪（x）就能在 p 分钟内找出“有毒”水桶？n只水桶里有且仅有一只有毒的桶。
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+    }
+};
