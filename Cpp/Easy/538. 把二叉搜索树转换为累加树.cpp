1547413c97b99e0187e6c5f2cc74ae1896b16da6
Mr.Huang <18757163093@163.com>
2018-07-26 21:39:09 +0800

    Create 538. 把二叉搜索树转换为累加树.cpp

---
+++ Cpp/Easy/538. 把二叉搜索树转换为累加树.cpp
@@ -0,0 +1,42 @@
+给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+
+例如：
+
+输入: 二叉搜索树:
+              5
+            /   \
+           2     13
+
+输出: 转换为累加树:
+             18
+            /   \
+          20     13
+          
+          
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    TreeNode* convertBST(TreeNode* root) {
+        convert(root);
+        return root;
+    }
+private:
+    void convert(TreeNode* cur) {
+        if(cur == NULL) {
+            return;
+        }
+        convert(cur->right);
+        cur->val += sum;
+        sum = cur->val;
+        convert(cur->left);
+    }
+};
