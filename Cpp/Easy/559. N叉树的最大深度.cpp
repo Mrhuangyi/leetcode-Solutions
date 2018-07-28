06580b9018052a5cbbd5826fa794d91eb73f8a1c
Mr.Huang <18757163093@163.com>
2018-07-28 14:17:06 +0800

    Create 559. N叉树的最大深度.cpp

---
+++ Cpp/Easy/559. N叉树的最大深度.cpp
@@ -0,0 +1,45 @@
+给定一个N叉树，找到其最大深度。
+
+最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+
+例如，给定一个 3叉树 :
+
+
+
+ 
+
+我们应返回其最大深度，3。
+
+说明:
+
+树的深度不会超过 1000。
+树的节点总不会超过 5000。
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(root == NULL) {
+            return 0;
+        }
+        int depth = 0;
+        for(auto child : root->children) {
+            depth = max(depth, maxDepth(child));
+        }
+        return depth + 1;
+    }
+};
