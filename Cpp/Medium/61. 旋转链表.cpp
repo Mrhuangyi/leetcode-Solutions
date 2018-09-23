353b9454621515a905039f7d3b29ff9d33dea606
Mr.Huang <18757163093@163.com>
2018-09-23 20:53:31 +0800

    Create 61. 旋转链表.cpp

---
+++ Cpp/Medium/61. 旋转链表.cpp
@@ -0,0 +1,50 @@
+给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+
+输入: 1->2->3->4->5->NULL, k = 2
+输出: 4->5->1->2->3->NULL
+解释:
+向右旋转 1 步: 5->1->2->3->4->NULL
+向右旋转 2 步: 4->5->1->2->3->NULL
+示例 2:
+
+输入: 0->1->2->NULL, k = 4
+输出: 2->0->1->NULL
+解释:
+向右旋转 1 步: 2->0->1->NULL
+向右旋转 2 步: 1->2->0->NULL
+向右旋转 3 步: 0->1->2->NULL
+向右旋转 4 步: 2->0->1->NULL
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || k == 0) {
+            return head;
+        }
+        int n = 1;
+        ListNode* p = head;
+        while(p->next) {
+            p = p->next;
+            n++;
+        }
+        k = n - k % n;
+        p->next = head;
+        for(int i = 0; i < k; i++) {
+            p = p->next;
+        }
+        head = p->next;
+        p->next = NULL;
+        return head;
+    } 
+};
