a5efdc2ea1691d851661d92f2cd42e7e3b391d5a
Mr.Huang <18757163093@163.com>
2018-09-24 18:15:01 +0800

    Create 86. 分隔链表.cpp

---
+++ Cpp/Medium/86. 分隔链表.cpp
@@ -0,0 +1,40 @@
+给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+
+你应当保留两个分区中每个节点的初始相对位置。
+
+示例:
+
+输入: head = 1->4->3->2->5->2, x = 3
+输出: 1->2->2->4->3->5
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode dummy1(0),dummy2(0);
+        ListNode* p1 = &dummy1;
+        ListNode* p2 = &dummy2;
+        ListNode* p = head;
+        while(p) {
+            if(p->val < x) {
+                p1->next = p;
+                p1 = p1->next;
+            } else {
+                p2->next = p;
+                p2 = p2->next;
+            }
+            p = p->next;
+        }
+        p2->next = NULL;
+        p1->next = dummy2.next;
+        return dummy1.next;
+    }
+};
