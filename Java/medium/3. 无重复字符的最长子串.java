b42d41e6caea8683aa6858283ab4b083f35ae898
Mr.Huang <18757163093@163.com>
2018-10-15 22:20:55 +0800

    Update 3. 无重复字符的最长子串.java

--- Java/medium/3. 无重复字符的最长子串.java
+++ Java/medium/3. 无重复字符的最长子串.java
@@ -1,3 +1,4 @@
+/*
 给定一个字符串，找出不含有重复字符的最长子串的长度。
 
 示例：
@@ -7,12 +8,11 @@
 给定 "bbbbb" ，最长的子串就是 "b" ，长度是 1。
 
 给定 "pwwkew" ，最长子串是 "wke" ，长度是 3。请注意答案必须是一个子串，"pwke" 是 子序列  而不是子串。
-
+*/
 解法一：暴力法
 枚举所有的子字符串，一个个检查相应的字符串是否含有重复的字符。检查一个字符串是否含有重复字符可以用set集合。便利整个字符串，将其中的字符一个个加入set，并且在加入之前先检查set里面是否已经有了该字符。
 时间复杂度O(n^3),空间复杂度O(min(n,m))
 
-```java
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
@@ -38,7 +38,7 @@ class Solution {
         return true;
     }
 }
-```
+
 
 > 上面的代码我测了下是超时的。
 
@@ -47,7 +47,7 @@ class Solution {
 利用HashSet作为滑动窗口，可以让检查的复杂度降为O(1)
 我们使用 HashSet 将字符存储在当前窗口 [i,j)（最初 j=i）中。 然后我们向右侧滑动索引 j，如果它不在 HashSet 中，我们会继续滑动 j。直到 s[j] 已经存在于 HashSet 中。此时，我们找到的没有重复字符的最长子字符串将会以索引 i 开头。
 
-```java
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
@@ -65,11 +65,11 @@ class Solution {
         return res;
     }
 }
-```
+
 
 解法三：用HashMap优化滑动窗口
 
-```java
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
@@ -86,4 +86,3 @@ class Solution {
         return res;
     }
 }
-```
