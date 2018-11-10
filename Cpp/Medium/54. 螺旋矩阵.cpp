9c6aec6ca80a9d67c34d30c018d6c27b97d5c48f
Mr.Huang <18757163093@163.com>
2018-11-10 21:17:40 +0800

    Create 54. 螺旋矩阵.cpp

---
+++ Cpp/Medium/54. 螺旋矩阵.cpp
@@ -0,0 +1,62 @@
+/*
+给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+
+示例 1:
+
+输入:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+输出: [1,2,3,6,9,8,7,4,5]
+示例 2:
+
+输入:
+[
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+]
+输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+*/
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if(matrix.empty()) {
+            return {};
+        }
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> res(m * n);
+        int u = 0, d = m - 1, l = 0, r = n - 1, k = 0;
+        while(true) {
+            for(int col = l; col <= r; col++) {
+                res[k++] = matrix[u][col];
+            }
+            if(++u > d) {
+                break;
+            }
+            for(int row = u; row <= d; row++) {
+                res[k++] = matrix[row][r];
+            }
+            if(--r < l) {
+                break;
+            }
+            for(int col = r; col >= l; col--) {
+                res[k++] = matrix[d][col];
+            }
+            if(--d < u) {
+                break;
+            }
+            for(int row = d; row >= u; row--) {
+                res[k++] = matrix[row][l];
+            }
+            if(++l > r) {
+                break;
+            }
+        }
+        return res;
+    }
+};
