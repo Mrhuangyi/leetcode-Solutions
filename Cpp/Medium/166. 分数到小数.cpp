6987a1eed414cb51df7ecac5153b0d4b4d06b20e
Mr.Huang <18757163093@163.com>
2018-11-22 21:07:05 +0800

    Create 166. 分数到小数.cpp

---
+++ Cpp/Medium/166. 分数到小数.cpp
@@ -0,0 +1,49 @@
+/*
+给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以字符串形式返回小数。
+
+如果小数部分为循环小数，则将循环的部分括在括号内。
+
+示例 1:
+
+输入: numerator = 1, denominator = 2
+输出: "0.5"
+示例 2:
+
+输入: numerator = 2, denominator = 1
+输出: "2"
+示例 3:
+
+输入: numerator = 2, denominator = 3
+输出: "0.(6)"
+*/
+
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        int s1 = numerator >= 0 ? 1 : -1;
+        int s2 = denominator >= 0 ? 1 : -1;
+        long long num = abs( (long long)numerator );
+        long long den = abs( (long long)denominator );
+        long long out = num / den;
+        long long rem = num % den;
+        unordered_map<long long, int> m;
+        string res = to_string(out);
+        if (s1 * s2 == -1 && (out > 0 || rem > 0)) res = "-" + res;
+        if (rem == 0) return res;
+        res += ".";
+        string s = "";
+        int pos = 0;
+        while (rem != 0) {
+            if (m.find(rem) != m.end()) {
+                s.insert(m[rem], "(");
+                s += ")";
+                return res + s;
+            }
+            m[rem] = pos;
+            s += to_string((rem * 10) / den);
+            rem = (rem * 10) % den;
+            ++pos;
+        }
+        return res + s;
+    }
+};
