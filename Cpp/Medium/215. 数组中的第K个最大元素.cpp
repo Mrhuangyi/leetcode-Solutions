da7b62420a1ef121f51acdd0227541878a88a790
Mr.Huang <18757163093@163.com>
2018-11-28 19:45:42 +0800

    Create 215. 数组中的第K个最大元素.cpp

---
+++ Cpp/Medium/215. 数组中的第K个最大元素.cpp
@@ -0,0 +1,70 @@
+/*
+在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+示例 1:
+
+输入: [3,2,1,5,6,4] 和 k = 2
+输出: 5
+示例 2:
+
+输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+输出: 4
+说明:
+
+你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+*/
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.rbegin(), nums.rend());
+        return nums[k - 1];
+    }
+};
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+       if (nums.size() == 0 || k < 1 || k > nums.size()){
+            return -1;
+        }
+        return partition(nums, 0, nums.size() - 1, nums.size() - k);
+    }
+    int partition(vector<int> &nums, int start, int end, int k) {
+        if (start >= end) {
+            return nums[k];
+        }
+        
+        int left = start, right = end;
+        int pivot = nums[(start + end) / 2];
+        
+        while (left <= right) {
+            while (left <= right && nums[left] < pivot) {
+                left++;
+            }
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+        
+        if (k <= right) {
+            return partition(nums, start, right, k);
+        }
+        if (k >= left) {
+            return partition(nums, left, end, k);
+        }
+        return nums[k];
+    }    
+    
+    void swap(vector<int> &nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+};
+
