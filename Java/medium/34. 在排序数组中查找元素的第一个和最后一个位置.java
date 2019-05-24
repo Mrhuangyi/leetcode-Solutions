de5e34c31b535a6865861cea2d11dc65ef57703b
Mr.Huang <18757163093@163.com>
2019-05-24 21:09:22 +0800

    Create 34. 在排序数组中查找元素的第一个和最后一个位置.java

---
+++ Java/medium/34. 在排序数组中查找元素的第一个和最后一个位置.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = binarySearch(nums, target);
+        int last = binarySearch(nums, target + 1) - 1;
+        if(first == nums.length || nums[first] != target) {
+            return new int[]{-1, -1};
+        } else {
+            return new int[]{first, Math.max(first, last)};
+        }
+    }
+    private int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
