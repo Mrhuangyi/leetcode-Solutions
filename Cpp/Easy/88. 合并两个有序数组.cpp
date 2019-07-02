735d49ef8979c8548baaf0f20e3f30a70aff2ed1
Mr.Huang <18757163093@163.com>
2019-07-02 15:00:07 +0800

    Update 88. 合并两个有序数组.cpp

--- Cpp/Easy/88. 合并两个有序数组.cpp
+++ Cpp/Easy/88. 合并两个有序数组.cpp
@@ -15,10 +15,20 @@ nums2 = [2,5,6],       n = 3
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=m,j=0;i<m+n,j<n;i++,j++){
-            nums1[i] = nums2[j];
+        int index1 = m - 1;
+        int index2 = n - 1;
+        int index3 = m + n - 1;
+        
+        while(index1 >= 0 || index2 >= 0) {
+            if(index1 < 0) {
+                nums1[index3--] = nums2[index2--];
+            } else if(index2 < 0) {
+                nums1[index3--] = nums1[index1--];
+            } else if(nums1[index1] > nums2[index2]) {
+                nums1[index3--] = nums1[index1--];
+            } else {
+                nums1[index3--] = nums2[index2--];
+            }
         }
-        int k = m+n;
-        sort(nums1.begin(),nums1.end());
     }
 };
