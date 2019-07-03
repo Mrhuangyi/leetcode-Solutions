b65b4726b5881575006c0735a791d36ef0e88909
Mr.Huang <18757163093@163.com>
2019-07-03 14:52:37 +0800

    Update 125. 验证回文串.cpp

--- Cpp/Easy/125. 验证回文串.cpp
+++ Cpp/Easy/125. 验证回文串.cpp
@@ -36,3 +36,20 @@ public:
         return true;
     }
 };
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j)
+        {
+            while(i<j && !isdigit(s[i]) && !isalpha(s[i]))i++;
+            while(i<j && !isdigit(s[j]) && !isalpha(s[j]))j--;
+            if(i<j && tolower(s[i])!=tolower(s[j]))
+                return false;
+            i++;j--;
+        }
+        return true;
+    }
+};
