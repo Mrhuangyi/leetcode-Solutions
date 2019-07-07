38e38a74cb3d33870cc86657368ff7f54af25e0e
Mr.Huang <18757163093@163.com>
2019-07-07 10:50:23 +0800

    Update 107. 二叉树的层次遍历 II.cpp

--- Cpp/Easy/107. 二叉树的层次遍历 II.cpp
+++ Cpp/Easy/107. 二叉树的层次遍历 II.cpp
@@ -28,6 +28,47 @@
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        TreeNode* node;
+        if(root == NULL) {
+            return res;
+        }
+        q.push(root);
+        while(!q.empty()) {
+            int width = q.size();
+            vector<int> temp;
+            for(int i = 0; i < width; i++) {
+                node = q.front();
+                temp.push_back(node->val);
+                q.pop();
+                if(node->left) {
+                    q.push(node->left);
+                }
+                if(node->right) {
+                    q.push(node->right);
+                }
+            }
+            res.push_back(temp);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
         int depth = getHeight(root);
         vector<vector<int>> ret(depth);
         if(depth==0){
