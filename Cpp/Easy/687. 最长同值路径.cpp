03985cd390d852e23a8162c656b0b62614a82bb9
Mr.Huang <18757163093@163.com>
2019-07-07 19:42:13 +0800

    Update 687. 最长同值路径.cpp

--- Cpp/Easy/687. 最长同值路径.cpp
+++ Cpp/Easy/687. 最长同值路径.cpp
@@ -29,6 +29,47 @@
 
 注意: 给定的二叉树不超过10000个结点。 树的高度不超过1000。
 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int res = 0;
+    int dfs(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        if(root->left != NULL && root->left->val == root->val) {
+            left++;
+        } else {
+            left = 0;
+        }
+        if(root->right != NULL && root->right->val == root->val) {
+            right++;
+        } else {
+            right = 0;
+        }
+        res = max(res, left + right);
+        return max(left, right);
+    }
+    int longestUnivaluePath(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
+        dfs(root);
+        return res;
+    }
+};
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
