2bcd8ab5c626f2a6b2e577a8782408614d0c87cc
Mr.Huang <18757163093@163.com>
2019-07-07 11:02:11 +0800

    Update 110. 平衡二叉树.cpp

--- Cpp/Easy/110. 平衡二叉树.cpp
+++ Cpp/Easy/110. 平衡二叉树.cpp
@@ -42,6 +42,38 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
+        if(root == NULL) {
+            return true;
+        }
+        int temp = abs(depth(root->left) - depth(root->right));
+        if(temp > 1) {
+            return false;
+        }
+        return isBalanced(root->left) && isBalanced(root->right);
+    }
+    int depth(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
+        int left = depth(root->left);
+        int right = depth(root->right);
+        return 1 + max(left, right);
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
         return balanceHeight(root)>=0;
     }
     int balanceHeight(TreeNode* root){
