21cf35fdec706915e886a1813a6d2b0b68e0f2dd
Mr.Huang <18757163093@163.com>
2019-07-07 11:06:13 +0800

    Update 111. 二叉树的最小深度.cpp

--- Cpp/Easy/111. 二叉树的最小深度.cpp
+++ Cpp/Easy/111. 二叉树的最小深度.cpp
@@ -29,6 +29,32 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+        if(left == 0 || right == 0) {
+            return max(left, right) + 1;
+        } else {
+        return 1 + min(left, right);
+        }
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
         return minDepth(root,false);
     }
 private:
