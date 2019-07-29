683bc826cefa89d32c8792b1f8bf31c538aad197
Mr.Huang <18757163093@163.com>
2019-07-29 16:00:18 +0800

    Update 204. 计数质数.cpp

--- Cpp/Easy/204. 计数质数.cpp
+++ Cpp/Easy/204. 计数质数.cpp
@@ -6,6 +6,8 @@
 输出: 4
 解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
 
+正解：埃士筛法
+
 class Solution {
 public:
     int countPrimes(int n) {
@@ -26,4 +28,32 @@ public:
         return cnt;
     }
   
+};
+
+暴力解法：
+超时：
+class Solution {
+public:
+    static bool isPrime(int n) {
+        if(n < 2) {
+            return false;
+        }
+        for(int i = 2; i <= sqrt(n); i++) {
+            if(n % i == 0) 
+                return false;
+        }
+        return true;
+    }
+    
+    int countPrimes(int n) {
+        int res = 0;
+        while(n) {
+            n--;
+            if(isPrime(n)) {
+                res++;
+            }
+           
+        }
+        return res;
+    }
 };
