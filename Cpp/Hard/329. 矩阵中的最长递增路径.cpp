2ffdcc9e093ca7b9922b535f906b72878e296c19
Mr.Huang <18757163093@163.com>
2019-08-20 20:12:18 +0800

    Create 329. 矩阵中的最长递增路径.cpp

---
+++ Cpp/Hard/329. 矩阵中的最长递增路径.cpp
@@ -0,0 +1,60 @@
+给定一个整数矩阵，找出最长递增路径的长度。
+
+对于每个单元格，你可以往上，下，左，右四个方向移动。 你不能在对角线方向上移动或移动到边界外（即不允许环绕）。
+
+示例 1:
+
+输入: nums = 
+[
+  [9,9,4],
+  [6,6,8],
+  [2,1,1]
+] 
+输出: 4 
+解释: 最长递增路径为 [1, 2, 6, 9]。
+示例 2:
+
+输入: nums = 
+[
+  [3,4,5],
+  [3,2,6],
+  [2,2,1]
+] 
+输出: 4 
+解释: 最长递增路径是 [3, 4, 5, 6]。注意不允许在对角线方向上移动。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-increasing-path-in-a-matrix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+class Solution {
+public:
+    vector<vector<int> > dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if(matrix.empty() || matrix[0].empty()) {
+            return 0;
+        }
+        int res = 1, m = matrix.size(), n = matrix[0].size();
+        vector<vector<int> > dp(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                res = max(res, dfs(matrix, dp, i, j));
+            }
+        }
+            return res;
+    }
+    
+    int dfs(vector<vector<int> > &matrix, vector<vector<int> > &dp, int i, int j) {
+        if (dp[i][j]) return dp[i][j];
+        int mx = 1, m = matrix.size(), n = matrix[0].size();
+        for (auto a : dirs) {
+            int x = i + a[0], y = j + a[1];
+            if (x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] <= matrix[i][j]) continue;
+            int len = 1 + dfs(matrix, dp, x, y);
+            mx = max(mx, len);
+        }
+        dp[i][j] = mx;
+        return mx;
+    }
+};
