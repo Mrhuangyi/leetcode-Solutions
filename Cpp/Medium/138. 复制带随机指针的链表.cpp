84f3763d489fbb0f058031048c30b89c7529272c
Mr.Huang <18757163093@163.com>
2019-10-09 19:42:40 +0800

    Update 138. 复制带随机指针的链表.cpp

--- Cpp/Medium/138. 复制带随机指针的链表.cpp
+++ Cpp/Medium/138. 复制带随机指针的链表.cpp
@@ -2,39 +2,44 @@
 
 要求返回这个链表的深度拷贝。 
 
-/**
- * Definition for singly-linked list with a random pointer.
- * struct RandomListNode {
- *     int label;
- *     RandomListNode *next, *random;
- *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
- * };
- */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node() {}
+
+    Node(int _val, Node* _next, Node* _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
 class Solution {
 public:
-    RandomListNode *copyRandomList(RandomListNode *head) {
-        if(head == NULL) {
-            return NULL;
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) {
+            return head;
         }
-        RandomListNode dummy(0);
-        RandomListNode* n = &dummy;
-        RandomListNode* h = head;
-        map<RandomListNode*, RandomListNode*> m;
-        while(h) {
-            RandomListNode* node = new RandomListNode(h->label);
-            n->next = node;
-            n = node;
-            node->random = h->random;
-            m[h] = node;
-            h = h->next;
+        Node *cur = head;
+        map<Node*, Node*> map;
+        //1. 遍历链表，将原节点作为key，拷贝节点作为value保存在map中
+        while (cur != nullptr) {
+            Node *copy = new Node(cur->val);
+            map[cur] = copy;
+            cur = cur->next;
         }
-        h = dummy.next;
-        while(h) {
-            if(h->random != NULL) {
-                h->random = m[h->random];
-            }
-            h = h->next;
+        //2. 复制链表next和random指针
+        cur = head;
+        while (cur != nullptr) {
+            map[cur]->next = map[cur->next];
+            map[cur]->random = map[cur->random];
+            cur = cur->next;
         }
-        return dummy.next;
+        return map[head];
     }
 };
