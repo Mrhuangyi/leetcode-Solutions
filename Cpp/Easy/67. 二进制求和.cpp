6b24c31ec4dc97c2d1c448d10c07a7576b1d7836
Mr.Huang <18757163093@163.com>
2019-10-16 13:40:06 +0800

    Update 67. 二进制求和.cpp

--- Cpp/Easy/67. 二进制求和.cpp
+++ Cpp/Easy/67. 二进制求和.cpp
@@ -11,7 +11,36 @@
 输入: a = "1010", b = "1011"
 输出: "10101"
 
-
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int len1 = a.size(), len2 = b.size();
+        if(len1 == 0) {
+            return b;
+        }
+        if(len2 == 0) {
+            return a;
+        }
+        string res;
+        int i = len1 - 1;
+        int j = len2 - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0) {
+            if(i >= 0) {
+                carry += a[i--] - '0';
+            }
+            if(j >= 0) {
+                carry += b[j--] - '0';
+            }
+            res += to_string(carry % 2);
+            carry >>= 1;
+        }
+        reverse(res.begin(), res.end());
+        return carry > 0 ? '1' + res : res;
+    }
+};
+    
+    
 class Solution {
 public:
     string addBinary(string a, string b) {
