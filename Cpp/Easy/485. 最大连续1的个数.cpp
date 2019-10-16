abd751b811e7d052e40d5dc3f3729755c49fb15e
Mr.Huang <18757163093@163.com>
2019-10-16 14:12:47 +0800

    Update 485. 最大连续1的个数.cpp

--- Cpp/Easy/485. 最大连续1的个数.cpp
+++ Cpp/Easy/485. 最大连续1的个数.cpp
@@ -13,6 +13,28 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
+        int res = 0;
+        if(nums.size() == 0) {
+            return res;
+        }
+        int temp = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == 1) {
+                temp++;
+            } else {
+                res = max(res, temp);
+                temp = 0;
+            }
+        }
+        res = max(res, temp);
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
         int maxm = 0,cnt = 0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
