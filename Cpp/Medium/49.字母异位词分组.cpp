9d474d018d84e7e5a4ab0483a213d24e829a3ecf
Mr.Huang <18757163093@163.com>
2019-12-21 21:00:56 +0800

    Create 49.字母异位词分组.cpp

---
+++ Cpp/Medium/49.字母异位词分组.cpp
@@ -0,0 +1,32 @@
+给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+
+示例:
+
+输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+输出:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+说明：
+
+所有输入均为小写字母。
+不考虑答案输出的顺序。
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string> > res;
+        map<string, vector<string> > Map;
+        for(int i = 0; i < strs.size(); i++) {
+            string key = strs[i];
+            sort(key.begin(), key.end());
+            Map[key].push_back(strs[i]);
+        }
+        for(auto it = Map.begin(); it != Map.end(); it++) {
+            res.push_back(it->second);
+        }
+        return res;
+    }
+};
