83503825bb070bf3f7041680f907920c67811b46
Mr.Huang <18757163093@163.com>
2019-12-24 19:43:42 +0800

    Update 287. 寻找重复数.cpp

--- Cpp/Medium/287. 寻找重复数.cpp
+++ Cpp/Medium/287. 寻找重复数.cpp
@@ -82,3 +82,17 @@ public:
         return -1;
     }
 };
+
+//排序后判断
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == nums[i+1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
