df6167b5951b1fd50b37f45340ad4b891cb6c3ed
Mr.Huang <18757163093@163.com>
2020-02-06 22:25:42 +0800

    Update 24. 两两交换链表中的节点.cpp

--- Cpp/Medium/24. 两两交换链表中的节点.cpp
+++ Cpp/Medium/24. 两两交换链表中的节点.cpp
@@ -10,6 +10,30 @@
 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head->next == NULL) {
+            return head;
+        }
+        ListNode* firstNode = head;
+        ListNode* secondNode = head->next;
+        
+        firstNode->next = swapPairs(secondNode->next);
+        secondNode->next = firstNode;
+        
+        return secondNode;
+    }
+};
+
 */
 
 //
