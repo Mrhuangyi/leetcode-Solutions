5c9daf53b853c51883e8bfd8af8a61591587d8bc
Mr.Huang <18757163093@163.com>
2020-03-08 13:51:11 +0800

    Create 322. 零钱兑换.cpp

---
+++ Cpp/Medium/322. 零钱兑换.cpp
@@ -0,0 +1,42 @@
+给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+
+示例 1:
+
+输入: coins = [1, 2, 5], amount = 11
+输出: 3 
+解释: 11 = 5 + 5 + 1
+示例 2:
+
+输入: coins = [2], amount = 3
+输出: -1
+说明:
+你可以认为每种硬币的数量是无限的。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/coin-change
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+       // 数组大小为 amount + 1，初始值也为 amount + 1
+    vector<int> dp(amount + 1, amount + 1);
+    dp[0] = 0;
+    for (int i = 0; i < dp.size(); i++) {
+        // 内层 for 在求所有子问题 + 1 的最小值
+        for (int coin : coins) {
+            // 子问题无解，跳过
+            if (i - coin < 0) {
+                continue;
+            }
+            dp[i] = min(dp[i], 1 + dp[i - coin]);
+        }
+    }
+    if(dp[amount] == amount + 1) {
+        return -1;
+    } else {
+        return dp[amount];
+    }
+    }
+};
