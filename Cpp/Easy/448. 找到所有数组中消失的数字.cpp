13ce4bf8884cbd00be01699fc9b3a8acb2748561
Mr.Huang <18757163093@163.com>
2020-03-09 17:48:22 +0800

    Update 448. 找到所有数组中消失的数字.cpp

--- Cpp/Easy/448. 找到所有数组中消失的数字.cpp
+++ Cpp/Easy/448. 找到所有数组中消失的数字.cpp
@@ -12,6 +12,24 @@
 输出:
 [5,6]
 
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> res;
+        for(int i = 0; i < nums.size(); i++) {
+            nums[abs(nums[i])-1] = -abs(nums[abs(nums[i])-1]);
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] > 0) {
+                res.push_back(i+1);
+            }
+        }
+        return res;
+    }
+};
+
+
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
